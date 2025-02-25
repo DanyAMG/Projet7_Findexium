a69619728b5846b176ba72fb884a8c9633a9c514
Dany MOTA <dany.mota.goncalves@gmail.com>
2025-02-25 18:37:31 +0100

    Création des entités et ajout de l'InitialCreate afin de crée la bdd

--- P7CreateRestApi/Data/LocalDbContext.cs
+++ P7CreateRestApi/Data/LocalDbContext.cs
@@ -1,11 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Controllers.Domain;
+using Dot.Net.WebApi.Controllers;
 
 namespace Dot.Net.WebApi.Data
 {
     public class LocalDbContext : DbContext
     {
         public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options) { }
+        public DbSet<BidList> Bids { get; set; }
+        public DbSet<CurvePoint> CurvePoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+        public DbSet<RuleName> Rules { get; set; }
+
+    
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
