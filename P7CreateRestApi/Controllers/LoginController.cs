76ab16d72d108a0ff64d01fe67b58959100edbb1
Dany MOTA <dany.mota.goncalves@gmail.com>
2025-03-04 02:15:52 +0100

    Rajout des packages IdentityFramework et des packages Authentification pour l'authentification par token.

--- P7CreateRestApi/Controllers/LoginController.cs
+++ P7CreateRestApi/Controllers/LoginController.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using P7CreateRestApi.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Dot.Net.WebApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class LoginController : ControllerBase
-    {             
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public LoginController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
         [HttpPost]
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
